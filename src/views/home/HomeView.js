1509fc98267097983a154e2c858bc4762a2ae721
matilameiro <mati.lameiro@gmail.com>
2021-05-30 19:07:23 -0300

    primer commit

---
+++ src/views/home/HomeView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeView = () => {
+    return (
+        <div>
+            <h1>Hola María Lorena Lampis</h1>
+        </div>
+    )
+};
+
+export default HomeView;
